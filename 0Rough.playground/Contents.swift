a26993b3d40da29c1f1f101e37a625aef94bf621
Ritik <ritik.k.1404@gmail.com>
2025-01-22 21:12:11 +0530

    ok

--- 0Rough.playground/Contents.swift
+++ 0Rough.playground/Contents.swift
@@ -1,17 +1,11 @@
-func lengthOfLastWord(_ s: String) -> Int {
-    var ans = 0
-    var count = 0
-    
-    for char in s.reversed() {
-        if char.isLetter {
-            ans += 1
-            count += 1
-        } else if count > 0 {
-            return ans
-        }
-    }
-    return ans
-    }
+func longestCommonPrefix(_ strs: [String]) -> String {
+        var arrayOfStrings = strs
+    for index in 0..<arrayOfStrings.count {
+        if c.first
 
+        }
+    
+    return "h"
+    }
 
 
